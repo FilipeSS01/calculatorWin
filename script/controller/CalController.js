2975429291793a48bf0025e413af0baa4f9dd7ce
Filipe Silva <filipe060701@gmail.com>
2022-06-10 17:46:34 -0300

    calculatorJS - Update index.html && Added new functions in CalcController.js

--- script/controller/CalController.js
+++ script/controller/CalController.js
@@ -6,6 +6,7 @@ class CalcController {
     this._operation = [];
 
     this.initialize();
+    this.displayCal = 0;
   }
 
   initialize() {
@@ -18,7 +19,6 @@ class CalcController {
   // ===============================================
 
   btnClick(btn) {
-    // console.log(btn);
     switch (btn) {
       case "0":
       case "1":
@@ -30,10 +30,20 @@ class CalcController {
       case "7":
       case "8":
       case "9":
+        this.addOperation(btn);
         break;
-    //   case "←":
-    //     console.log(btn);
-    //     break;
+      case "plus":
+        this.addOperation("+");
+        break;
+      case "less":
+        this.addOperation("-");
+        break;
+      case "c":
+        this.clear();
+        break;
+      case "ce":
+        this.clearEntry();
+        break;
 
       default:
         break;
@@ -43,9 +53,64 @@ class CalcController {
   btnEvents() {
     document.querySelectorAll(".btn").forEach((btn) => {
       btn.addEventListener("click", () => {
-        this.btnClick(btn.innerHTML);
+        let btnInfo = btn.classList[3].replace("btn-", "").toString();
+        this.btnClick(btnInfo);
       });
     });
+  }
+
+  // ===============================================
+  //                 Settings
+  // ===============================================
+
+  addOperation(value) {
+    if (isNaN(this.getLastOperation())) {
+      if (this.isOperator(value)) {
+        this.setLastOperation(value);
+      } else {
+        this.pushOperation(value);
+      }
+    } else {
+      if (this.isOperator(value)) {
+        this.pushOperation(value);
+      } else {
+        let newValue = this.getLastOperation().toString() + value.toString();
+        this.setLastOperation(newValue);
+      }
+    }
+    this.updateDisplay();
+  }
+
+  clear() {
+    this._operation = [];
+    this.displayCal = 0;
+  }
+
+  clearEntry() {
+    if (!this.isOperator(this.getLastOperation())) {
+      this._operation.pop();
+    }
+    this.displayCal = 0;
+  }
+
+  pushOperation(value) {
+    this._operation.push(value);
+  }
+
+  isOperator(value) {
+    return ["+", "-", "*", "%", "/"].indexOf(value) > -1;
+  }
+
+  getLastOperation() {
+    return this._operation[this._operation.length - 1];
+  }
+
+  setLastOperation(value) {
+    this._operation[this._operation.length - 1] = value;
+  }
+
+  updateDisplay() {
+    this.displayCal = this._operation.join(" ");
   }
 
   // ===============================================
